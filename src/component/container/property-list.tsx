0a7df1ea5f48e4dd6d05d460d7c61f6f0c23c4af
Julius Walther <juliuswalther1991@googlemail.com>
2018-04-25 17:39:45 +0200

    feat(property-list): show type selector

--- src/component/container/property-list.tsx
+++ src/component/container/property-list.tsx
@@ -125,13 +125,28 @@ class PropertyTree extends React.Component<PropertyTreeProps> {
 					const type = property.getType();
 					const value = this.getValue(id, context && context.path);
 
+					const propTypes = [
+						{
+							id: type,
+							name: type
+						},
+						{
+							id: 'yolo',
+							name: 'Boolean'
+						},
+						{
+							id: 'slot',
+							name: 'Slot'
+						}
+					];
+
 					switch (type) {
 						case 'boolean':
 							return (
 								<PropertyItem
 									propertyName={name}
 									selectedPropertyType=""
-									propertyTypes={[]}
+									propertyTypes={propTypes}
 								>
 									<BooleanItem
 										key={id}
@@ -146,7 +161,7 @@ class PropertyTree extends React.Component<PropertyTreeProps> {
 								<PropertyItem
 									propertyName={name}
 									selectedPropertyType=""
-									propertyTypes={[]}
+									propertyTypes={propTypes}
 								>
 									<StringItem
 										key={id}
@@ -169,7 +184,7 @@ class PropertyTree extends React.Component<PropertyTreeProps> {
 								<PropertyItem
 									propertyName={name}
 									selectedPropertyType=""
-									propertyTypes={[]}
+									propertyTypes={propTypes}
 								>
 									<EnumItem
 										key={id}
@@ -188,7 +203,7 @@ class PropertyTree extends React.Component<PropertyTreeProps> {
 								<PropertyItem
 									propertyName={name}
 									selectedPropertyType=""
-									propertyTypes={[]}
+									propertyTypes={propTypes}
 								>
 									<AssetItem
 										key={id}
@@ -216,7 +231,7 @@ class PropertyTree extends React.Component<PropertyTreeProps> {
 								<PropertyItem
 									propertyName={name}
 									selectedPropertyType=""
-									propertyTypes={[]}
+									propertyTypes={propTypes}
 								>
 									<PropertyTree key={id} context={newContext} element={element} />
 								</PropertyItem>
@@ -227,7 +242,7 @@ class PropertyTree extends React.Component<PropertyTreeProps> {
 								<PropertyItem
 									propertyName={name}
 									selectedPropertyType=""
-									propertyTypes={[]}
+									propertyTypes={propTypes}
 								>
 									<div key={id}>Unknown type: {type}</div>
 								</PropertyItem>
